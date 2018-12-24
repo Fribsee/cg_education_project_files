12efe62d98e43e4711018d99e9f61be595864bbd
fribsee <40807715+Fribsee@users.noreply.github.com>
2018-12-24 13:54:32 -0600

    added prompt to determine department

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ console.log(
 `Teacher: ${teacherName}
 Department: ${department}
 Ratings: ${rating1.toFixed(1)}, ${rating2.toFixed(1)}, ${rating3.toFixed(1)}`);
+
 let avgRating = (rating1 + rating2 + rating3)/3;
 console.log(`Avg. Rating: ${avgRating.toFixed(5)}`);
 
@@ -21,3 +22,55 @@ console.log(`Student Name: ${studentName}
   GPA: ${gpa}`);
 console.log(`Department: ${courseDept}
   Course: ${courseTitle}`);
+
+  let teacherRatings = [rating1,rating2,rating3];
+
+
+
+   function enterTeacherRating(){
+     return Number(prompt('We would like for you to review. Please enter a rating between 0.0 and 5.0.'))};
+
+    let newRating = enterTeacherRating();
+
+  while(!newRating || newRating < 0 || newRating >5){
+      newRating = enterTeacherRating()};
+
+
+function addTeacherRating(array, rating){
+  return array.push(rating)};
+
+addTeacherRating(teacherRatings, newRating);
+
+let getRatingAvg =
+  teacherRatings.reduce((accumulator, currentValue)=>{
+   return accumulator + currentValue / teacherRatings.length}, 0);
+
+alert("Thanks for your review! " + teacherName + " average rating is now " + getRatingAvg);
+console.log(teacherRatings);
+
+let courses = [
+  ['Silly running', 'Physical Education'],
+  ['Art 101', 'Art'],
+  ['Potato peeling', 'Culinary'],
+  ['Pop-locking', 'Physical Education'],
+  ['Body Latexing', 'Art'],
+  ['Noodle sculpting', 'Culinary']
+];
+let enterDept = ()=> {return prompt('What department are you looking for a course in?')};
+let whatDept = enterDept().toLowerCase();
+switch(whatDept){
+  case 'art':
+  break;
+  case 'physical education':
+  break;
+  case 'culinary':
+  break;
+  default:
+  enterDept();
+}
+
+
+// function course_Dept(course, depart){
+//   return courses.filter(depart)
+// };
+// console.log course__dept()
